e09228c1d14aa63ba50c9be509ae7fb3a7c83b73
hasitha <hasithadkr7@gmail.com>
2018-04-18 10:36:04 +0530

    changes

--- FilesToBucket.py
+++ FilesToBucket.py
@@ -4,9 +4,13 @@
 from google.cloud import storage
 import json
 import sys
+import string
+import random
+from datetime import datetime
+
 
 # Buckets/curwsl_nfs_1/results/wrf0_2018-04-15_18:00_f2Nq
-# Buckets/curwsl_nfs_1/results/hec_hms_2018-04-15
+# Buckets/curwsl_nfs_1/results/hec_hms0_2018-04-15_18:00
 # inflow
 # outflow
 # /hec-hms/FLO2D/INFLOW.DAT
@@ -15,19 +19,21 @@ import sys
 
 def upload_hec_hms_files():
     print("upload_hec_hms_files...")
+    time = datetime.now().strftime("%H:%M")
+    path_suffix = '_'+time+'_'+''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(4))
     try:
         client = storage.Client.from_service_account_json(key_file)
         try:
             bucket = client.get_bucket(bucket_name)
             print("Upload INFLOW.DAT to bucket...")
             try:
-                inflow_blob = bucket.blob(bucket_name+'/results/hec_hms_'+forecast_date+'/inflow/INFLOW.DAT')
+                inflow_blob = bucket.blob(bucket_name+'/results/hec_hms0_'+forecast_date+path_suffix+'/inflow/INFLOW.DAT')
                 inflow_blob.upload_from_filename(filename=inflow_dat)
             except Exception as ex1:
                 print("Upload INFLOW.DAT|Exception: ", ex1)
             print("Upload OUTFLOW.DAT to bucket...")
             try:
-                outflow_blob = bucket.blob(bucket_name+'/results/hec_hms_'+forecast_date+'/outflow/OUTFLOW.DAT')
+                outflow_blob = bucket.blob(bucket_name+'/results/hec_hms0_'+forecast_date+path_suffix+'/outflow/OUTFLOW.DAT')
                 outflow_blob.upload_from_filename(filename=outflow_dat)
             except Exception as ex2:
                 print("Upload OUTFLOW.DAT|Exception: ", ex2)
